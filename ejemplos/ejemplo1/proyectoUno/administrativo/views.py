cb2c341136fb74855290716c4680c9e13e2a0b52
JoseUrdiales <jlurdiales1@utpl.edu.ec>
2024-07-11 20:14:01 -0500

    plantilla tablas

--- ejemplos/ejemplo1/proyectoUno/administrativo/views.py
+++ ejemplos/ejemplo1/proyectoUno/administrativo/views.py
@@ -20,14 +20,9 @@ def index(request):
         Listar los registros del modelo Estudiante,
         obtenidos de la base de datos.
     """
-    # a través del ORM de django se obtiene
-    # los registros de la entidad; el listado obtenido
-    # se lo almacena en una variable llamada
-    # estudiantes
+
     estudiantes = Estudiante.objects.all()
-    # en la variable tipo diccionario llamada informacion_template
-    # se agregará la información que estará disponible
-    # en el template
+
     informacion_template = {'estudiantes': estudiantes, 'numero_estudiantes': len(estudiantes)}
     return render(request, 'index.html', informacion_template)
 
